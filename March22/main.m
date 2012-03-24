3bf6bb14a505ccc2583ff8bb817221a5dc251ef2
Joonho Lee <jlee@n30i.org>
2012-03-24 15:42:21 -0400

    first commit

--- March22/main.m
+++ March22/main.m
@@ -12,6 +12,17 @@
 
 int main(int argc, char *argv[])
 {
+    int i = 10;
+    CGFloat f = 3.14159;
+    UIDevice *device = [UIDevice currentDevice];
+    NSString *version = device.systemVersion;
+   
+    NSLog(@"This is output from Joonho Lee March22");
+    NSLog(@"The value of i is %d.",i);
+    NSLog(@"The value of float is %g.",f);
+    NSLog(@"iOS version %@",version);
+    
+                        
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([March22AppDelegate class]));
     }
