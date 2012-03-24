a14fcfb221b3921aeaa54ed747979823e9be30d5
Joonho Lee <jlee@n30i.org>
2012-03-24 13:46:46 -0400

    Initial Commit

---
+++ March22/March22AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  March22AppDelegate.h
+//  March22
+//
+//  Created by J Lee on 3/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface March22AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ March22/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  March22
+//
+//  Created by J Lee on 3/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "March22AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([March22AppDelegate class]));
+    }
+}
